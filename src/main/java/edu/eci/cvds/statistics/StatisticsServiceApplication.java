ca3003d120cf66d7a9d9d02309c0326d6bfa1775
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2025-04-24 03:28:32 -0500

    New Files by Jesus Pinzon

---
+++ src/main/java/edu/eci/cvds/statistics/StatisticsServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.statistics;
+
+/**
+ * Hello world!
+ *
+ */
+public class StatisticsServiceApplication {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
