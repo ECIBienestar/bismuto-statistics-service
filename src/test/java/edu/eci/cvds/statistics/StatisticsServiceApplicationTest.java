cd2fc44d020f5d8314d8a6b49a3616e8cccb2964
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2025-04-28 23:38:49 -0500

    Deployment Test using GitHub Actions

---
+++ src/test/java/edu/eci/cvds/statistics/StatisticsServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package edu.eci.cvds.statistics;
+
+import org.junit.jupiter.api.Test;
+
+class StatisticsServiceApplicationTest {
+
+    @Test
+    void simpleTest() {
+        assert true;
+    }
+}
